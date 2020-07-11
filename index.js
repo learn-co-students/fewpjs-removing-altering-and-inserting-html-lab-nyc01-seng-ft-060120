0f441ab3c012638420f33c5aa89bc0587a9df904
mayalexandera <maya.blevins@gmail.com>
2020-07-10 23:10:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const word = "boobs";
+console.log(word);
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = 'maya is the champion';
+newHeader.className = 'whatshappening'

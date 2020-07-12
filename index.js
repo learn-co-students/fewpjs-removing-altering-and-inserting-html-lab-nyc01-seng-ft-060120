ac2d837d74530df1d22731dee24c48eae5bf7f95
shiro-han <shan99business@gmail.com>
2020-07-12 19:46:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// let mainElement = document.getElementById("main")
+let mainElement = document.querySelector('main')
+mainElement.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "S+ Hiro is the champion!"
+document.body.appendChild(newHeader)

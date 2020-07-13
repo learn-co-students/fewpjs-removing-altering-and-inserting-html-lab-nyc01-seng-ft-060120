7e68b3161c2d90930c934ef54524a1040eb44956
alexpac27 <alexpac27@gmail.com>
2020-07-13 00:06:20 -0400

    Done.

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ main.remove()
 let newHeader = document.createElement("h1")
 newHeader.id = "victory"
 newHeader.innerHTML = "YOUR_NAME is the champion"
-newHeader.className = "victory";
+// newHeader.className = "victory";
 
